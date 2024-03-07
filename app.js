f9ab7e7d2910575ed72eb66c351ba26f75421c17
niranjan <niranjanchougule17@gmail.com>
2024-03-07 21:44:34 +0530

    test

--- app.js
+++ app.js
@@ -152,6 +152,10 @@ app.get("/course-detail", verifyToken, async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
